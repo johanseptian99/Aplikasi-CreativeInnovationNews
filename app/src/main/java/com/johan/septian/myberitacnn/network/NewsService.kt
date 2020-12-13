304187985a53c71347344f490cdd57cac967b056
Irfan Arsyananda <71767382+IrfanArsyananda@users.noreply.github.com>
2020-12-13 21:28:54 +0700

    update NewsService

--- app/src/main/java/com/johan/septian/myberitacnn/network/NewsService.kt
+++ app/src/main/java/com/johan/septian/myberitacnn/network/NewsService.kt
@@ -11,6 +11,34 @@ interface NewsService {
     @GET("/")
     fun getBeritaRandom() : Call<Berita>
 
+    //getDataNasional
+    @GET("nasional")
+    fun getDataNasional() : Call<Berita>
+
+    //getDataInternasional
+    @GET("internasional")
+    fun getDataInternasional() : Call<Berita>
+
+    //getDataEkonomi
+    @GET("ekonomi")
+    fun getDataEkonomi() : Call<Berita>
+
+    //getDataOlahraga
+    @GET("olahraga")
+    fun getDataOlahraga() : Call<Berita>
+
+    //getDataTeknologi
+    @GET("teknologi")
+    fun getDataTeknologi() : Call<Berita>
+
+    //getDataHiburan
+    @GET("hiburan")
+    fun getDataHiburan() : Call<Berita>
+
+    //getDataGayaHidup
+    @GET("gaya-hidup")
+    fun getDataGayaHidup() : Call<Berita>
+
     @GET("search/")
     fun getSearchBerita(
         @Query("q") query: String?) : Call<Berita>
