96e62806dfc0c9a6532d288be748c53d4c4d5d8e
Irfan Arsyananda <71767382+IrfanArsyananda@users.noreply.github.com>
2020-12-13 21:46:01 +0700

    rev

--- app/src/main/java/com/johan/septian/myberitacnn/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/johan/septian/myberitacnn/ui/dashboard/DashboardFragment.kt
@@ -5,27 +5,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.johan.septian.myberitacnn.R
+import kotlinx.android.synthetic.main.fragment_dashboard.view.*
 
 class DashboardFragment : Fragment() {
 
     private lateinit var dashboardViewModel: DashboardViewModel
 
-    override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         dashboardViewModel =
                 ViewModelProvider(this).get(DashboardViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
-//        val textView: TextView = root.findViewById(R.id.text_dashboard)
-//        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
-//            textView.text = it
-//        })
+
+        root.cardCategoryNasional.setOnClickListener {
+            Toast.makeText(context, "Membuka Berita Nasional", Toast.LENGTH_SHORT).show()
+        }
+
+        root.cardCategoryInternasional.setOnClickListener {
+            Toast.makeText(context, "Membuka Berita Internasional", Toast.LENGTH_SHORT).show()
+        }
+
         return root
     }
 }
